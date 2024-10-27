74dafa00f08a120b87051f009b6771ea56ef8a08
Dara Casey <dara.e.casey.25@dartmouth.edu>
2024-10-27 18:08:35 -0400

    added navigation between screens

---
+++ src/screens/Archive.js
@@ -0,0 +1,11 @@
+// src/screens/Archive.js
+import React from 'react';
+
+const Archive = () => (
+  <div>
+    <h1>Archive Page</h1>
+    <p>Welcome to the Archive Page!</p>
+  </div>
+);
+
+export default Archive;
---
+++ src/screens/Trips.js
@@ -0,0 +1,11 @@
+// src/screens/Trips.js
+import React from 'react';
+
+const Trips = () => (
+  <div>
+    <h1>Trips Page</h1>
+    <p>Welcome to the Trips Page!</p>
+  </div>
+);
+
+export default Trips;
