aba3a97eabfff6240e73ebb0719291b2f328c34b
Sammy Rago <samantha.k.rago.25@dartmouth.edu>
2024-11-10 21:36:37 -0500

    Update AddTrip.js

--- src/screens/AddTrip.js
+++ src/screens/AddTrip.js
@@ -17,28 +17,9 @@ const AddTrip = () => {
     trip_leader: '',
     trip_capacity: 0,
     subclub: '',
-    trip_location: '',
-    trip_provided: '',
-    trip_bring: '',
-    trip_type: '',
-    trip_level: '',
   });
 
   const [subclubs, setSubclubs] = useState([]);
-
-  const tripTypeOptions = [
-    {value: '', label: 'Select Trip Type'},
-    {value: 'day_trip', label: 'Day Trip'},
-    {value: 'overnight_trip', label: 'Overnight'},
-  ];
-
-  const tripLevelOptions = [
-    {value: '', label: 'Select Trip Level'},
-    {value: 'beginner', label: 'Beginner'},
-    {value: 'intermediate', label: 'Intermediate'},
-    {value: 'advanced', label: 'Advanced'},
-  ];
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -91,75 +72,36 @@ const AddTrip = () => {
     }
   };
 
-  // return (
-  //   <div className="app-container">
-  //     <div className="add-trip-container">
-  //       <h2 className='add-trip-title'>Plan a Trip</h2>
-  //       <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
-  //         <div className="form-question">
-  //           <label className='form-label'>Title</label>
-  //           <input type="text" className="form-input" id="title" name="trip_name" value={formData.trip_name} onChange={handleChange} required />
-  //         </div>
-  //         <div className="shared-line">
-  //           <div className="form-question" id="date-group">
-  //             <label className='form-label'>Date</label>
-  //             <input type="date" className="form-input" id="date" name="trip_date" value={formData.trip_date} onChange={handleChange} required />
-  //           </div>
-  //           <div className="form-question" id="leader-group">
-  //             <label className='form-label'>Trip Leader</label>
-  //             <input type="text" className="form-input" id="leader" name="trip_leader" value={formData.trip_leader} onChange={handleChange} required />
-  //           </div>
-  //         </div>
-  //         <div className="shared-line">
-  //           <div className="form-question" id="capacity-group">
-  //             <label className='form-label'>Capacity</label>
-  //             <input type="number" className="form-input" id="capacity" name="trip_capacity" value={formData.trip_capacity} onChange={handleChange} required />
-  //           </div>
-  //           <label htmlFor="subclub">Subclub:</label>
-  //           <select name="subclub" value={formData.subclub} onChange={handleChange} required>
-  //             <option value="">Select a Subclub</option>
-  //             {subclubs.map(subclub => (
-  //               <option key={subclub.id} value={subclub.id}>{subclub.subclub_name}</option>
-  //             ))}
-  //           </select>
-  //         </div>
-  //         <div className="form-question">
-  //           <label className='form-label'>Description</label>
-  //           <textarea className="form-input" id="description" name="trip_description" value={formData.trip_description} onChange={handleChange} required />
-  //         </div>
-  //         <button type="submit" className="submit-button">Create</button>
-
-  //       </form>
-  //     </div>
-  //   </div>
-  // );
-// };
-return (
-  <div className="app-container">
-    <div className="add-trip-container">
-      <h2 className='add-trip-title'>Plan a Trip</h2>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <div className="form-question">
-          <label className='form-label'>Title</label>
-          <input type="text" className="form-input" id="title" name="trip_name" value={formData.trip_name} onChange={handleChange} required />
-        </div>
-        <div className="shared-line">
-          <div className="form-question" id="date-group">
-            <label className='form-label'>Date</label>
-            <input type="date" className="form-input" id="date" name="trip_date" value={formData.trip_date} onChange={handleChange} required />
+  return (
+    <div className="app-container">
+      <div className="add-trip-container">
+        <h2 className='add-trip-title'>Plan a Trip</h2>
+        <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+          <div className="form-question">
+            <label className='form-label'>Title</label>
+            <input type="text" className="form-input" id="title" name="trip_name" value={formData.trip_name} onChange={handleChange} required />
           </div>
-          <div className="form-question" id="leader-group">
-            <label className='form-label'>Trip Leader</label>
-            <input type="text" className="form-input" id="leader" name="trip_leader" value={formData.trip_leader} onChange={handleChange} required />
+          <div className="shared-line">
+            <div className="form-question" id="date-group">
+              <label className='form-label'>Date</label>
+              <input type="date" className="form-input" id="date" name="trip_date" value={formData.trip_date} onChange={handleChange} required />
+            </div>
+            <div className="form-question" id="leader-group">
+              <label className='form-label'>Trip Leader</label>
+              <input type="text" className="form-input" id="leader" name="trip_leader" value={formData.trip_leader} onChange={handleChange} required />
+            </div>
           </div>
-        </div>
-        <div className="shared-line">
+          <div className="shared-line">
             <div className="form-question" id="capacity-group">
               <label className='form-label'>Capacity</label>
               <input type="number" className="form-input" id="capacity" name="trip_capacity" value={formData.trip_capacity} onChange={handleChange} required />
             </div>
+            {/* <div className="form-question" id="subclub-group">
+              <label className='form-label'>Subclub</label>
+              <input type="text" className="form-input" id="subclub" name="subclub" value={formData.subclub} onChange={handleChange} required />
+            </div> */}
             <label htmlFor="subclub">Subclub:</label>
-            <select name="subclub" value={formData.subclub} onChange={handleChange} required className="form-input">
+            <select name="subclub" value={formData.subclub} onChange={handleChange} required>
               <option value="">Select a Subclub</option>
               {subclubs.map(subclub => (
                 <option key={subclub.id} value={subclub.id}>{subclub.subclub_name}</option>
@@ -170,37 +112,8 @@ return (
             <label className='form-label'>Description</label>
             <textarea className="form-input" id="description" name="trip_description" value={formData.trip_description} onChange={handleChange} required />
           </div>
-          <div className="shared-line">
-            <div className="form-question" id="trip-type-group">
-              <label className='form-label'>Trip Type</label>
-              <select name="trip_type" value={formData.trip_type} onChange={handleChange} required className="form-input">
-                {tripTypeOptions.map(option => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="form-question" id="trip-level-group">
-              <label className='form-label'>Trip Level</label>
-              <select name="trip_level" value={formData.trip_level} onChange={handleChange} required className="form-input">
-                {tripLevelOptions.map(option => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="form-question">
-            <label className='form-label'>What to Bring</label>
-            <textarea className="form-input" id="what-to-bring" name="trip_bring" value={formData.trip_bring} onChange={handleChange} />
-          </div>
-          <div className="form-question">
-            <label className='form-label'>Provided Items</label>
-            <textarea className="form-input" id="provided" name="trip_provided" value={formData.trip_provided} onChange={handleChange} />
-          </div>
           <button type="submit" className="submit-button">Create</button>
+
         </form>
       </div>
     </div>
